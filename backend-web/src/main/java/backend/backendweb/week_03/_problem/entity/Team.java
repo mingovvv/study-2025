f4508d4723eea37adeaae04efb7575461fd9b470
yh.Jung <dusgh123123@gmail.com>
2025-05-21 20:48:09 +0900

    feat: entity data 어노테이션 추가

--- backend-web/src/main/java/backend/backendweb/week_03/_problem/entity/Team.java
+++ backend-web/src/main/java/backend/backendweb/week_03/_problem/entity/Team.java
@@ -3,6 +3,7 @@ package backend.backendweb.week_03._problem.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Table(name = "TEAM")
 @Entity
+@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
